8a9568c5fc2281cac2e67d696a450a8114ef8524
Mona Chung <monalizards@gmail.com>
2021-07-18 16:51:14 +0100

    Change server dockerfile configs

--- server/bert_model_setup.py
+++ server/bert_model_setup.py
@@ -1,16 +1,24 @@
 # used with docker to download models
+from json import load
 from transformers import BertTokenizer, BertForQuestionAnswering
 from transformers import LongformerTokenizer, LongformerForQuestionAnswering
 
-print('Loading bert tokenizer')
-tokenizer = BertTokenizer.from_pretrained(
-    'deepset/bert-large-uncased-whole-word-masking-squad2')
-print('Loading bert for question answering')
-model = BertForQuestionAnswering.from_pretrained(
-    'deepset/bert-large-uncased-whole-word-masking-squad2')
-print('Loading longformer tokenizer')
-tokenizer = LongformerTokenizer.from_pretrained(
-    "allenai/longformer-large-4096-finetuned-triviaqa")
-print('Loading longformer for question answering')
-model = LongformerForQuestionAnswering.from_pretrained(
-    "allenai/longformer-large-4096-finetuned-triviaqa")
+
+def load_bert():
+    print('Loading bert tokenizer')
+    tokenizer = BertTokenizer.from_pretrained(
+        'deepset/bert-large-uncased-whole-word-masking-squad2')
+    print('Loading bert for question answering')
+    model = BertForQuestionAnswering.from_pretrained(
+        'deepset/bert-large-uncased-whole-word-masking-squad2')
+    print('Loading longformer tokenizer')
+    tokenizer = LongformerTokenizer.from_pretrained(
+        "allenai/longformer-large-4096-finetuned-triviaqa")
+    print('Loading longformer for question answering')
+    model = LongformerForQuestionAnswering.from_pretrained(
+        "allenai/longformer-large-4096-finetuned-triviaqa")
+
+
+if __name__ == "__main__":
+    load_bert()
+    print('Bert models loaded')
