3af86a88f802625e278e1afbff760d71e4fefb9c
Mona Chung <monalizards@gmail.com>
2021-07-22 21:06:13 +0100

    Add youtube transcript api as fallback to pytube

---
+++ server/services/youtube_caption_helper.py
@@ -0,0 +1,19 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+def find_lang_caption(videoId, languages=['en']):
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(videoId)
+        transcript = transcript_list.find_transcript(languages).fetch()
+        return transcript
+    except Exception as e:
+        print(e)
+        print("No transcription found.")
+        return None
+
+
+if __name__ == "__main__":
+    print("Example transcript: ")
+    videoId = 'bQI5uDxrFfA'
+    transcript = find_lang_caption(videoId)
+    print(transcript)
